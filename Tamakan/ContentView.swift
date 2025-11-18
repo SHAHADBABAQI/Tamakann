427ae97f5db3f79dcf42dc406c8f0d843eeaa2d3
shahad <shada-123@hotmail.com>
2025-11-18 10:24:19 +0300

    Initial Commit

---
+++ Tamakan/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Tamakan
+//
+//  Created by shahad khaled on 27/05/1447 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Tamakan/TamakanApp.swift
@@ -0,0 +1,17 @@
+//
+//  TamakanApp.swift
+//  Tamakan
+//
+//  Created by shahad khaled on 27/05/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct TamakanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
